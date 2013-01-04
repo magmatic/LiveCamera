f7ef71fc80f02f4f370b772dcfe842be39b7fbbc
M <marina@milkshake.local>
2013-01-04 17:34:20 -0500

    Initial Commit

---
+++ LiveCamera/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LiveCamera
+//
+//  Created by M on 2013-01-04.
+//  Copyright (c) 2013 Black Magma Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LiveCamera/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LiveCamera
+//
+//  Created by M on 2013-01-04.
+//  Copyright (c) 2013 Black Magma Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
